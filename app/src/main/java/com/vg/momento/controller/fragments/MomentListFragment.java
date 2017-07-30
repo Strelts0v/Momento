3d6924bbae144f1f7b7dd0dd268de1ccf63da769
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-07-30 21:43:41 +0300

    add class MomentListFragment

---
+++ app/src/main/java/com/vg/momento/controller/fragments/MomentListFragment.java
@@ -0,0 +1,155 @@
+package com.vg.momento.controller.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import com.vg.momento.R;
+import com.vg.momento.controller.activities.MomentActivity;
+import com.vg.momento.dao.implementations.EmbeddedMomentDao;
+import com.vg.momento.dao.interfaces.MomentDao;
+import com.vg.momento.model.Moment;
+import java.util.List;
+
+public class MomentListFragment extends Fragment {
+
+    private RecyclerView mMomentRecycleView;
+
+    private MomentAdapter mAdapter;
+
+    private MomentDao mMomentDao;
+
+    private static final int REQUEST_CODE_MOMENT = 1;
+
+    private static final String KEY_LAST_UPDATED_MOMENT_POSITION = "last_updated_moment";
+
+    public static MomentListFragment newInstance() {
+        // Initializing of bundle for fragment
+        Bundle args = new Bundle();
+
+        MomentListFragment fragment = new MomentListFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_moment_list, container, false);
+
+        mMomentRecycleView = (RecyclerView) view.findViewById(R.id.moment_recycler_view);
+
+        // RecycleView delegates LayoutManager management of widgets
+        // LinearLayoutManager dispose elements using vertical list
+        mMomentRecycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // Initialize MomentDao object
+        mMomentDao = EmbeddedMomentDao.getInstance();
+
+        updateUI();
+
+        return view;
+    }
+
+    private void updateUI() {
+        List<Moment> moments = mMomentDao.getAllMoments();
+
+        if (mAdapter == null) {
+            mAdapter = new MomentAdapter(moments);
+            mMomentRecycleView.setAdapter(mAdapter);
+        } else {
+            // update only item that could be updated
+            int position = getArguments().getInt(KEY_LAST_UPDATED_MOMENT_POSITION, 0);
+            mAdapter.notifyItemChanged(position);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_MOMENT) {
+            // Обработка результата
+        }
+    }
+
+    private class MomentHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener{
+
+        private Moment mMoment;
+
+        private TextView mTitleTextView;
+
+        private TextView mDateTextView;
+
+        private CheckBox mIsFavoriteCheckBox;
+
+        public MomentHolder(View itemView) {
+            super(itemView);
+
+            mTitleTextView = (TextView) itemView.findViewById(
+                    R.id.list_item_moment_title_text_view);
+            mDateTextView = (TextView) itemView.findViewById(
+                    R.id.list_item_moment_date_text_view);
+            mIsFavoriteCheckBox = (CheckBox) itemView.findViewById(
+                    R.id.list_item_moment_is_favorite_check_box);
+
+            itemView.setOnClickListener(this);
+        }
+
+        public void bindMoment(Moment moment){
+            mMoment = moment;
+            mTitleTextView.setText(mMoment.getTitle());
+            mDateTextView.setText(mMoment.getDate().toLocaleString());
+            mIsFavoriteCheckBox.setChecked(mMoment.isFavorite());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = MomentActivity.newIntent(getActivity(), mMoment.getId());
+            if(mAdapter != null) {
+                getArguments().putInt(KEY_LAST_UPDATED_MOMENT_POSITION,
+                        mAdapter.mMoments.indexOf(mMoment));
+            }
+            startActivityForResult(intent, REQUEST_CODE_MOMENT);
+        }
+    }
+
+    private class MomentAdapter extends RecyclerView.Adapter<MomentHolder> {
+
+        private List<Moment> mMoments;
+
+        public MomentAdapter(List<Moment> moments) {
+            mMoments = moments;
+        }
+
+        @Override
+        public MomentHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater
+                    .inflate(R.layout.list_item_moment, parent, false);
+            return new MomentHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MomentHolder holder, int position) {
+            Moment moment = mMoments.get(position);
+            holder.bindMoment(moment);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mMoments.size();
+        }
+    }
+}
