1ff3d2189db28d77f56577596349e84310897d8d
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-08-03 12:25:24 +0300

    add class MomentEntry

---
+++ app/src/main/java/com/vg/momento/dao/entries/MomentEntry.java
@@ -0,0 +1,17 @@
+package com.vg.momento.dao.entries;
+
+import android.provider.BaseColumns;
+
+/* Inner class that defines the table contents */
+public abstract class MomentEntry implements BaseColumns {
+
+    public static final String TABLE_NAME = "moments";
+
+    public static final String COLUMN_NAME_ENTRY_ID = "moment_id";
+
+    public static final String COLUMN_NAME_TITLE = "title";
+
+    public static final String COLUMN_NAME_DATE = "date";
+
+    public static final String COLUMN_NAME_IS_FAVORITE = "is_favorite";
+}
